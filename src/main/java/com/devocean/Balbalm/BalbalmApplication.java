618ca8d39fabd54fc8a5b6a9a87b1e352e7b7568
devyubin <garmalhy1@gmail.com>
2024-10-14 17:44:07 +0900

    init project

---
+++ src/main/java/com/devocean/Balbalm/BalbalmApplication.java
@@ -0,0 +1,13 @@
+package com.devocean.Balbalm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalbalmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalbalmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devocean/Balbalm/BalbalmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devocean.Balbalm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalbalmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
