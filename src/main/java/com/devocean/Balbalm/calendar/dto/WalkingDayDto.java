8c8dcc844833d60e2c7711a7f71225a3f8d4877b
yubin <garmalhy1@naver.com>
2024-11-16 19:18:16 +0900

    Chore: edit DTO (#1)
    - dto 워딩 일부 수정
    
    Create temp-deploy.yml
    
    Del: delete temp deploy

--- src/main/java/com/devocean/Balbalm/calendar/dto/WalkingDayDto.java
+++ src/main/java/com/devocean/Balbalm/calendar/dto/WalkingDayDto.java
@@ -1,11 +1,15 @@
 package com.devocean.Balbalm.calendar.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalkingDayDto {
-	private int date;
-	private boolean hasWalking;
+	private int day;
+	private boolean isWalkingDay;
 }
--- src/main/java/com/devocean/Balbalm/calendar/dto/resp/CalendarResponseDto.java
+++ src/main/java/com/devocean/Balbalm/calendar/dto/resp/CalendarResponseDto.java
@@ -1,13 +1,22 @@
 package com.devocean.Balbalm.calendar.dto.resp;
 
+import java.util.List;
+
+import com.devocean.Balbalm.calendar.dto.WalkingDayDto;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CalendarResponseDto {
 	private int year;
 	private int month;
+	private List<WalkingDayDto> walkingDays;
 	private int totalWalkingDays;
 	private int firstDayOfWeek;
 }
