22cfcc9de2f46e153e800722f0948f5860c80970
Sim-mi-gyeong <smegyeong@gmail.com>
2024-11-23 16:46:05 +0900

    Feat: API 스웨거 설정 추가

--- src/main/java/com/devocean/Balbalm/dummy/controller/DummyController.java
+++ src/main/java/com/devocean/Balbalm/dummy/controller/DummyController.java
@@ -3,6 +3,7 @@ package com.devocean.Balbalm.dummy.controller;
 import com.devocean.Balbalm.dummy.usecase.GetDummyPlaceUseCase;
 import com.devocean.Balbalm.dummy.usecase.SaveDummyPlaceUseCase;
 import com.devocean.Balbalm.global.exception.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,11 +16,13 @@ public class DummyController {
     private final SaveDummyPlaceUseCase saveDummyPlaceUseCase;
     private final GetDummyPlaceUseCase getDummyPlaceUseCase;
 
+    @Operation(summary = "추천 장소 저장(관리자용 API)")
     @PostMapping("/place-recommend")
     public CommonResponse<SaveDummyPlaceUseCase.Result> saveDummyPlaceRecommend(@RequestBody SaveDummyPlaceUseCase.Command command) {
         return new CommonResponse<>(saveDummyPlaceUseCase.execute(command));
     }
 
+    @Operation(summary = "추천 장소 조회")
     @GetMapping("/place-recommend")
     public CommonResponse<GetDummyPlaceUseCase.Result> getDummyPlaceRecommend() {
         return new CommonResponse<>(getDummyPlaceUseCase.execute(null));
